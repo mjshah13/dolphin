f5c109c08400985fbb038f2f8dc40448abaf92c4
Shaarif <shaarifshahid@gmail.com>
2022-02-24 20:32:12 +0500

    modified

--- src/Components/RoadMap/RoadMap.jsx
+++ src/Components/RoadMap/RoadMap.jsx
@@ -3,20 +3,17 @@ import styles from './RoadMap.module.scss';
 import FirstImg from '../../Assets/first-img.png';
 import SecondImg from '../../Assets/last-img.png';
 import Line from '../../Assets/line.png';
-import RoadMap1 from '../../Assets/roadmap1.png';
-import RoadMap2 from '../../Assets/roadmap2.png';
-import RoadMap3 from '../../Assets/roadmap3.png';
-import RoadMap4 from '../../Assets/roadmap4.png';
-import RoadMap5 from '../../Assets/roadmap5.png';
+import RoadMap1 from '../../Assets/roadmap1.svg';
+import RoadMap2 from '../../Assets/roadmap2.svg';
+import RoadMap3 from '../../Assets/roadmap3.svg';
+import RoadMap4 from '../../Assets/roadmap4.svg';
+import RoadMap5 from '../../Assets/roadmap.svg';
 import Underline from '../../Assets/underline.png';
 
 const RoadMap = () => {
   return (
     <div className={styles.main_container}>
       <div className={styles.container}>
-        {/* <div className={styles.first_image_container}>
-          <img src={FirstImg} alt='first-img' />
-        </div> */}
         <div className={styles.div}>
           <div className={styles.heading_container}>
             <h1>
@@ -46,10 +43,6 @@ const RoadMap = () => {
             <img src={Underline} alt='underline' />
           </div>
         </div>
-
-        {/* <div className={styles.second_image_container}>
-          <img src={SecondImg} alt='second-img' />
-        </div> */}
       </div>
     </div>
   );
