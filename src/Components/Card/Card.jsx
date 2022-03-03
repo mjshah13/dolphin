d4724d951dc55070b819fa84df66cf0420e82e33
AbdulSaboor100 <abdulsaboor5808@gmail.com>
2022-03-03 15:17:21 +0500

    cards updated

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -8,16 +8,44 @@ const Card = () => {
     <div className={styles.main_cards}>
       <Row className={styles.innerDiv}>
         <Col sm={12} md={12}>
-          <CardsComponent img='Assets/Card1.png' mobileImg="Assets/cardmobile1.png" tilled="left" />
+          <CardsComponent
+            img='Assets/Card1.png'
+            mobileImg='Assets/cardmobile1.png'
+            tilled='left'
+            name='Virtual Real Estate'
+            rightText='Lease for Ad'
+            leftText='Land owner'
+          />
         </Col>
         <Col sm={12} md={12}>
-          <CardsComponent img='Assets/Card2.png' mobileImg="Assets/cardmobile2.png" tilled="right" />
+          <CardsComponent
+            img='Assets/Card2.png'
+            mobileImg='Assets/cardmobile2.png'
+            tilled='right'
+            name='Virtual Brick & Motar'
+            rightText='Lease for Ad'
+            leftText='Land owner'
+          />
         </Col>
         <Col sm={12} md={12}>
-          <CardsComponent img='Assets/Card3.png' mobileImg="Assets/cardmobile3.png" tilled="left" />
+          <CardsComponent
+            img='Assets/Card3.png'
+            mobileImg='Assets/cardmobile3.png'
+            tilled='left'
+            name='Event Hosting'
+            rightText='Lease for Ad'
+            leftText='Land owner'
+          />
         </Col>
         <Col sm={12} md={12}>
-          <CardsComponent img='Assets/Card4.png' mobileImg="Assets/cardmobile4.png" tilled="right" />
+          <CardsComponent
+            img='Assets/Card4.png'
+            mobileImg='Assets/cardmobile4.png'
+            tilled='right'
+            name='Virtual Brick & Motar'
+            rightText='Lease for Ad'
+            leftText='Land owner'
+          />
         </Col>
       </Row>
     </div>
--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,15 +1,52 @@
+// import React from 'react';
+// import styles from './Cards.module.scss';
+
+// const CardsComponent = ({ img, tilled, mobileImg }) => {
+//   return (
+//     <div className={styles.cards}>
+//       <div className={`${tilled === "left" ? styles.right : styles.left}`}>
+//         <img src={img} alt='cards' className={styles.desktopImg}/>
+//         <img src={mobileImg} alt="mobilecards" className={styles.mobileImg} />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CardsComponent;
+
 import React from 'react';
 import styles from './Cards.module.scss';
+import './OverRide.scss';
 
-const CardsComponent = ({ img, tilled, mobileImg }) => {
+const Cards = ({ img, tilled, mobileImg, name, rightText, leftText }) => {
   return (
-    <div className={styles.cards}>
-      <div className={`${tilled === "left" ? styles.right : styles.left}`}>
-        <img src={img} alt='cards' className={styles.desktopImg}/>
-        <img src={mobileImg} alt="mobilecards" className={styles.mobileImg} />
+    <div className={`${tilled === 'left' ? styles.right : styles.left}`}>
+      <div className={`${styles.cards} cards`}>
+        <div className={styles.cardsImg}>
+          <img className={styles.desktopImg} src={img} alt='' />
+          <img className={styles.mobileImg} src={mobileImg} alt='' />
+          <div className={styles.cardsArea}>
+            <div className={styles.cardsInfo}>
+              <h3>{name}</h3>
+            </div>
+            <div className={`${styles.buttons} buttons`}>
+              <div>
+                <p>
+                  <img src='Assets/leftbutton.png' alt='' />
+                  {leftText}
+                </p>
+              </div>
+              <div>
+                <p>
+                  <img src='Assets/rightbutton.png' alt='' /> {rightText}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default CardsComponent;
+export default Cards;
